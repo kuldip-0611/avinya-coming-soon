23d0dfb6d937ad29a137743a7e88c0ad4b5e073e
kuldip_trentiums <kuldip@trentiums.com>
2024-11-09 17:43:56 +0530

    update:ui for coming soon is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/logo.png";
+import backgroundImage from "./assets/images/background.png";
+import impactImage from "./assets/images/ourImpact.png";
+import "./App.css";
+import ContactUsForm from "./components/ContactUs";
+import LinkedIn from "./assets/icons/LinkedIn";
+import Instagram from "./assets/icons/Instagram";
+import EmailIcon from "./assets/icons/Email";
+import { Button, Col, Container, Image, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="bg-background text-foreground min-vh-100 d-flex flex-column justify-content-start responsive-padding">
+      <Container
+        fluid
+        className="responsive-padding shadow-all-sides rounded-2"
+      >
+        <Row className="gx-5">
+          <Col
+            xs={12}
+            lg={8}
+            className="d-flex flex-column align-items-start gap-4 order-2 order-lg-1"
+          >
+            <div className="mb-4 logo-responsive">
+              <Image
+                src={logo}
+                alt="Avinya Logo"
+                className="rounded-2"
+                width={100}
+                height={100}
+              />
+            </div>
+
+            <h1 className="text-center text-lg-start w-100">
+              <span className=" fw-bold heading-text">AVINYA</span> is coming
+              soon
+            </h1>
+
+            <h3 className=" description-text">
+              We’re working hard behind the scenes to bring you a new standard
+              in innovative plant-based vegan leather.
+            </h3>
+            <p className="description-text w-100">
+              Thank you for your patience as we finalize our digital home.
+            </p>
+
+            <ContactUsForm />
+
+            <div className="d-flex justify-content-center justify-content-lg-start gap-3 mt-4">
+              <Button
+                href="https://www.instagram.com/avinya_leather/"
+                target="_blank"
+                variant="link"
+                className="text-gray-700 hover:text-primary"
+              >
+                <Instagram />
+              </Button>
+              <Button
+                href="https://www.linkedin.com/company/avinya-leather/"
+                target="_blank"
+                variant="link"
+                className="text-gray-700 hover:text-primary"
+              >
+                <LinkedIn />
+              </Button>
+              <Button
+                href="mailto:connect@avinyaleather.com"
+                target="_blank"
+                variant="link"
+                className="text-gray-700 hover:text-primary"
+              >
+                <EmailIcon />
+              </Button>
+            </div>
+          </Col>
+
+          <Col
+            xs={12}
+            lg={4}
+            className="d-flex justify-content-center align-items-center order-1 order-lg-2"
+          >
+            <div className="d-flex h-100 flex-column align-items-center justify-content-center">
+              <div className="mb-4 h-100 upper-logo">
+                <Image
+                  src={logo}
+                  alt="Avinya Logo"
+                  className="rounded-2"
+                  width={100}
+                  height={100}
+                />
+              </div>
+
+              <div className="w-100 h-100 mb-4 d-lg-none">
+                <Image
+                  src={backgroundImage}
+                  alt="Background"
+                  className="w-100 h-100 object-cover rounded-2"
+                  width={100}
+                  height={100}
+                />
+              </div>
+
+              <div className="w-100 h-100 mb-4 logo-responsive">
+                <Image
+                  src={impactImage}
+                  alt="Impact Image"
+                  className="w-100 h-100 object-cover rounded-lg"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </main>
   );
 }
 
---
+++ src/components/ContactUs.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Form, Button, InputGroup } from "react-bootstrap";
+import toast from "react-hot-toast";
+import { FaPaperPlane } from "react-icons/fa";
+
+export default function ContactForm() {
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    console.log("inside submit");
+    e.preventDefault();
+    if (!message) {
+      toast.error("please enter message");
+    } else {
+      window.location.href = `mailto:connect@avinyaleather.com?subject=Inquiry%20to%20Avinya&body=${encodeURIComponent(
+        message
+      )}`;
+      toast.success("You will redirected to your email box!");
+    }
+  };
+
+  return (
+    <Form
+      onSubmit={handleSubmit}
+      className="d-flex align-items-center w-100 p-2 rounded-pill border border-gray-300"
+    >
+      <InputGroup className="w-100">
+        <Form.Control
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value.trimStart())}
+          placeholder="Enter your message"
+          className="flex-grow-1 px-4 py-2 bg-transparent border-0 placeholder-gray-400 text-sm custom-input"
+        />
+
+        <Button
+          type="submit"
+          className="p-3 bg-gradient-to-r from-primary to-secondary text-white rounded-circle transform scale-110"
+          style={{ background: "linear-gradient(90deg, #466645, #1C3628)" }}
+        >
+          <FaPaperPlane color="white" />
+        </Button>
+      </InputGroup>
+    </Form>
+  );
+}
